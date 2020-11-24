43d9c4cec36b20fd75d3a9be68d93f98e856d245
RFX9870 <rru2005@gmail.com>
2020-11-24 13:06:04 +0700

    deposit limit

--- src/util/depositInc.js
+++ src/util/depositInc.js
@@ -3,7 +3,8 @@ module.exports = function(client){
     setInterval(async() => {
         const balances = (await balance.findAll()).filter(b => b.deposit != 0)
         for(const bal of balances){
-            const sum = Math.round(bal.deposit*0.01)
+            let sum = Math.round(bal.deposit*0.01)
+            if(bal.deposit >= 250000) sum = 0
             await bal.update({deposit: bal.deposit+sum})
             console.log(`Incremented deposit - ${client.users.has(bal.userID) ? client.users.get(bal.userID).tag : bal.userID}, on ${sum}, new dep: ${bal.deposit}`)
         }
