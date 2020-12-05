78bb6f41c9e06f091ee413e96aa9cb36e61fb62a
RFX9870 <rru2005@gmail.com>
2020-12-05 17:35:33 +0700

    invite command update

--- src/commands/invite.js
+++ src/commands/invite.js
@@ -10,76 +10,65 @@ module.exports = {
         if(!args[0]) return await message.channel.createMessage(`> :x: **Использование:** \`${prefix}${this.name} ${this.usage}\``)
         const inv = args[0].slice(args[0].lastIndexOf("/")+1)
         client.getInvite(inv, true).then(async invite => {
+            const {guild} = invite
             const verLvl = ["Отсутствует", "Низкий", "Средний", "Высокий", "Самый высокий"]
-            const embed = {
-                title: invite.guild.name,
-                fields: [
-                    {
-                        name: "ID",
-                        value: invite.guild.id,
-                        inline: true
-                    },
-                    {
-                        name: "Уровень проверки",
-                        value: verLvl[invite.guild.verificationLevel],
-                        inline: true
-                    },
-                    {
-                        name: "Участники",
-                        value: `${invite.memberCount} (${invite.presenceCount} онлайн)`,
-                        inline: true
-                    }
-                ],
-                thumbnail: {url: `https://cdn.discordapp.com/icons/${invite.guild.id}/${invite.guild.icon}.jpg?size=1024`},
-                footer: {text: `Канал: #${invite.channel.name} (ID: ${invite.channel.id})`},
-                color: embColor
+            const now = Date.now()
+            const createdDaysAgo = Math.round((now - guild.createdAt) / (1000 * 60 * 60 * 24))
+            const owner = client.users.get(guild.ownerID)
+            const features = {
+                INVITE_SPLASH: "фон приглашения",
+                VIP_REGIONS: "384 кб/с голосовой канал",
+                VANITY_URL: "свой инвайт-код",
+                VERIFIED: "верифицированный сервер",
+                PARTNERED: "партнерский сервер",
+                PUBLIC: "публичный сервер",
+                COMMERCE: "поммерческий сервер",
+                NEWS: "новостной канал",
+                DISCOVERABLE: "доступен в обзоре серверов",
+                FEATURABLE: "предлагаемый сервер",
+                ANIMATED_ICON: "анимированный значок",
+                BANNER: "баннер",
+                PUBLIC_DISABLED: "не может быть публичным",
+                WELCOME_SCREEN_ENABLED: "экран приветствия",
+                COMMUNITY: "сервер сообщества"
             }
-            if(client.guilds.get(invite.guild.id)){
-                const now = Date.now()
-                const guild = client.guilds.get(invite.guild.id)
-                const owner = client.users.get(guild.ownerID)
-                const createdDaysAgo = Math.round((now - guild.createdAt) / (1000 * 60 * 60 * 24))
-                embed.fields.splice(2, 1)
-                embed.fields.push(
-                    {
-                        name: "Создан",
-                        value: `${strftime(`%d.%m.%Y в %H:%M:%S (${createdDaysAgo} дн. назад)`, new Date(guild.createdAt))}`
-                    },
-                    {
-                        name: "Владелец",
-                        value: `${owner.username}#${owner.discriminator}`
-                    },
-                    {
-                        name: "Регион",
-                        value: guild.region[0].toUpperCase() + guild.region.slice(1)
-                    },
-                    {
-                        name: "Участники: Люди | Боты | Всего",
-                        value: `${guild.members.filter(m => !m.user.bot).length} | ${guild.members.filter(m => m.user.bot).length} | ${guild.members.size}`
-                    },
-                    {
-                        name: "Каналы: Категории | Текстовые | Голосовые | Всего",
-                        value: `${guild.channels.filter(c => c.type == 4).length} | ${guild.channels.filter(c => c.type == 0).length} | ${guild.channels.filter(c => c.type == 2).length} | ${guild.channels.size}`
-                    },
-                    {
-                        name: "Эмоджи: Статичные | Анимированные | Всего",
-                        value: `${guild.emojis.filter(e => !e.animated).length} | ${guild.emojis.filter(e => e.animated).length} | ${guild.emojis.length}`
-                    },
-                    {
-                        name: "Всего ролей:",
-                        value: guild.roles.size
-                    }
-                )
-            }
-            if(invite.inviter){
-                embed.fields.push({
-                    name: "Приглашающий",
-                    value: `${invite.inviter.username}#${invite.inviter.discriminator} (ID: ${invite.inviter.id})`
-                })
-            }
-            return await message.channel.createMessage({embed})
-        }).catch(async () => {
-            return await message.channel.createMessage("> :x: Такого приглашения не существует.")
+            const embed = new Eris.Embed()
+            .title(guild.name)
+            .field("ID", guild.id, true)
+            if(client.guilds.has(guild.id)) embed.field("Владелец", owner.tag, true)
+            embed.field("Создан", strftime(`%d %B %Y года в %H:%M:%S (${createdDaysAgo} дн. назад)`, new Date(guild.createdAt)), true)
+            if(client.guilds.has(guild.id)) embed.field("Участники", 
+            `Всего - ${guild.members.size}
+Ботов - ${guild.members.filter(m => m.user.bot).length}
+            
+Онлайн - ${guild.members.filter(m => m.status == "online").length}
+Не активен - ${guild.members.filter(m => m.status == "idle").length}
+Не беспокоить - ${guild.members.filter(m => m.status == "dnd").length}
+Не в сети - ${guild.members.filter(m => m.status == "offline" || !m.status).length}`, true)
+                    .field("Каналы", 
+            `Всего - ${guild.channels.size}
+            
+Категории - ${guild.channels.filter(c => c.type == 4).length}
+Текстовые - ${guild.channels.filter(c => c.type == 0).length}
+Голосовые - ${guild.channels.filter(c => c.type == 2).length}
+Новостные - ${guild.channels.filter(c => c.type == 5).length}`, true)
+                    .field("Эмодзи", 
+            `Всего - ${guild.emojis.length}
+            
+Анимированные - ${guild.emojis.filter(e => e.animated).length}
+Статичные - ${guild.emojis.filter(e => !e.animated).length}`, true)
+                    .field("Ролей", guild.roles.size, true)
+                    .field("Регион", guild.region[0].toUpperCase() + guild.region.slice(1), true)
+                    embed.field("Уровень проверки", verLvl[guild.verificationLevel], true)
+                    .field("Функции", guild.features.map(f => features[f]).join(", ") || "Нет", true)
+                    .color(embColor)
+                    .thumbnail(guild.iconURL || "https://cdn.discordapp.com/embed/avatars/0.png")
+                    if(invite.channel) embed.footer(`Канал: #${invite.channel.name}`)
+                    if(invite.inviter) embed.author(`Приглашающий: ${invite.inviter.tag}`, invite.inviter.avatarURL)
+                    return await message.channel.createEmbed(embed)
+        }).catch(async error => {
+            if(error instanceof Eris.DiscordRESTError) return await message.channel.createMessage("> :x: Такого приглашения не существует.")
+            else throw error
         })
     }
 }
