f8c57e52ce2446a515bbdd39444ed3b35b44cc43
RFX9870 <rru2005@gmail.com>
2020-12-15 15:38:05 +0700

    reload update

--- src/commands/reload.js
+++ src/commands/reload.js
@@ -4,23 +4,28 @@ module.exports = {
     ownerOnly: true,
     async execute(client, message, args, prefix, embColor){
         switch(args[0]){
-            case "all":
-                const ma = await message.channel.createMessage(`Перезагрузка команд...`)
-                for(const cmd of client.commands.values()){
+            case "all":{
+                const msg = await message.channel.createMessage(`Перезагрузка команд...`)
+                const fs = require("fs")
+                client.commands.clear()
+                const commandFiles = fs.readdirSync("./src/commands").filter(f=>f.endsWith(".js")).map(f=>f.replace(".js", ""))
+                for(const cmd of commandFiles){
                     try{
-                        delete require.cache[require.resolve(`./${cmd.name}`)]
+                        delete require.cache[require.resolve(`./${cmd}`)]
+                        const command = require(`./${cmd}`)
+                        client.commands.set(command.name, command)
                     }catch(error){
-                        await message.channel.createEmbed({
+                        return await message.channel.createEmbed({
                             title: `Ошибка при перезагрузке ${cmd.name}`,
                             description: `\`\`\`js\n${error}\`\`\``,
                             color: embColor
                         })
                     }
                 }
-                await ma.edit(`Команды перезагружены!`)
-            break
-            case "events":
-                const me = await message.channel.createMessage(`Перезагрузка эвентов...`)
+                return await msg.edit(`Команды перезагружены!`)
+            }
+            case "events":{
+                const msg = await message.channel.createMessage(`Перезагрузка эвентов...`)
                 const fs = require("fs")
                 const listenerFiles = fs.readdirSync("./src/events").filter(f=>f.endsWith(".js")).map(f=>f.replace(".js", ""))
                 for(const file of listenerFiles){
@@ -38,35 +43,36 @@ module.exports = {
                         })
                     }
                 }
-                await me.edit(`Эвенты перезагружены!`)
-            break
-            case "config":
+                await msg.edit(`Эвенты перезагружены!`)
+            }
+            case "config":{
                 try{
                     delete require.cache[require.resolve(`../JSON/config.json`)]
                     global.config = require("../JSON/config.json")
                     await message.channel.createMessage("Настройки перезагружены!")
                 }catch(error){
-                    await message.channel.createEmbed({
+                    return await message.channel.createEmbed({
                         title: `Ошибка при перезагрузке настроек`,
                         description: `\`\`\`js\n${error}\`\`\``,
                         color: embColor
                     })
                 }
-            break
-            default:
+            }
+            default:{
                 if(!client.commands.has(args[0])) return await message.channel.createMessage("Такой команды нет")
                 try{
                     delete require.cache[require.resolve(`./${args[0]}`)]
-                    client.commands.set(args[0], require(`./${args[0]}`))
-                    await message.channel.createMessage(`Команда \`${args[0]}\` перезагружена!`)
+                    const command = require(`./${args[0]}`)
+                    client.commands.set(command.name, command)
+                    return await message.channel.createMessage(`Команда \`${command.name}\` перезагружена!`)
                 }catch(error){
-                    await message.channel.createEmbed({
+                    return await message.channel.createEmbed({
                         title: `Ошибка при перезагрузке ${args[0]}`,
                         description: `\`\`\`js\n${error}\`\`\``,
                         color: embColor
                     })
                 }
-            break
+            }
         }
     }
 }
