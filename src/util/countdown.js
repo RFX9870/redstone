29bc1ac43e940cffd08cf0e1843696fc7ac5156d
RFX9870 <rru2005@gmail.com>
2020-12-24 15:10:14 +0700

    added hours to countdown

--- src/util/countdown.js
+++ src/util/countdown.js
@@ -1,8 +1,9 @@
 module.exports = function(client){
     const endh = 60 - new Date().getMinutes()
     const fn = () => {
-        const days = Math.floor((1609459200000 - Date.now()) / 86400000)
-        client.editStatus(client.presence.status, {name: `${config.prefix}help || @${client.user.username} 🎄 ${days}d`, type: 5})
+        const days = Math.ceil((1609448400000 - Date.now()) / 86400000)
+        const hours = Math.ceil((1609448400000 - Date.now()) % 86400000 / 3600000)
+        client.editStatus(client.presence.status, {name: `${config.prefix}help || @${client.user.username} 🎄 ${days}d ${hours}h`, type: 5})
     }
     fn()
     setTimeout(() => {
