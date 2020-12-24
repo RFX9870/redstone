2dc90cda41988af87d63c878f801ebd1963ea73a
RFX9870 <rru2005@gmail.com>
2020-12-24 16:14:36 +0700

    using floor instead ceil

--- src/util/countdown.js
+++ src/util/countdown.js
@@ -1,8 +1,8 @@
 module.exports = function(client){
     const endh = 60 - new Date().getMinutes()
     const fn = () => {
-        const days = Math.ceil((1609448400000 - Date.now()) / 86400000)
-        const hours = Math.ceil((1609448400000 - Date.now()) % 86400000 / 3600000)
+        const days = Math.floor((1609448400000 - Date.now()) / 86400000)
+        const hours = Math.floor((1609448400000 - Date.now()) % 86400000 / 3600000)
         client.editStatus(client.presence.status, {name: `${config.prefix}help || @${client.user.username} 🎄 ${days}d ${hours}h`, type: 5})
     }
     fn()
