a8279610160a77907d6c0f84005062fe9be437af
RFX9870 <rru2005@gmail.com>
2020-12-30 11:51:07 +0700

    added commit to stats cmd

--- src/commands/stats.js
+++ src/commands/stats.js
@@ -9,6 +9,11 @@ module.exports = {
         const uptime = new Date(new Date(process.uptime() * 1000).toLocaleString("en-US", {timeZone: "Etc/Greenwich"}))
         const uptime_c = new Date(new Date(client.uptime).toLocaleString("en-US", {timeZone: "Etc/Greenwich"}))
         const uptime_h = new Date(new Date(require("os").uptime() * 1000).toLocaleString("en-US", {timeZone: "Etc/Greenwich"}))
+        let commit
+        try{
+            const ref = require("fs").readFileSync("./.git/HEAD").toString().slice(5).trim()
+            commit = require("fs").readFileSync(`./.git/${ref}`).toString().slice(0, 7)
+        }catch{}
         const embed = {
             title: "Статистика",
             fields: [
@@ -29,7 +34,7 @@ module.exports = {
                     value: `**Node.js:** ${process.version}\n**Eris:** ${require("eris").VERSION}\n**sqlite3:** ${require("sqlite3").VERSION}\n**Sequelize:** ${require("sequelize").version}`,
                 }
             ],
-            footer: {text: `${client.user.username} v${config.version} © RFX9870`, icon_url: client.user.avatarURL},
+            footer: {text: `${client.user.username} v${config.version} ${commit ? `(${commit})` : ""} © RFX9870`, icon_url: client.user.avatarURL},
             color: embColor
         }
         if(args[0] == "-v") {
