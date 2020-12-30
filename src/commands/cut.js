f4f32b311daa453c2f0e5e4fa627af9dca760878
RFX9870 <rru2005@gmail.com>
2020-12-30 12:27:20 +0700

    fixes

--- src/commands/cut.js
+++ src/commands/cut.js
@@ -3,8 +3,8 @@ module.exports = {
     group: "dev",
     async execute(client, message, args, prefix, embColor){
         if(!message.author.pos1 || !message.author.pos2) return await message.channel.createMessage("а где позиции епта")
-        return await message.channel.createMessage("да ты че... бляяяяяяяяяяяяяяяяяяяяяять!!!!")
         message.author.pos1 = false
         message.author.pos2 = false
+        return await message.channel.createMessage("да ты че... бляяяяяяяяяяяяяяяяяяяяяять!!!!")
     }
 }
--- src/commands/set.js
+++ src/commands/set.js
@@ -2,6 +2,6 @@ module.exports = {
     name: "set",
     group: "dev",
     async execute(client, message, args, prefix, embColor){
-        if(args[0] == "0")  await client.commands.get("cut").execute(client, message, args)
+        if(args[0] == "0") return await client.commands.get("cut").execute(client, message, args)
     }
 }
