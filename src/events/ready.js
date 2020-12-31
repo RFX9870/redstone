273aa9a8e104bf5e2f2dce1da6bb8168318f4ead
RFX9870 <rru2005@gmail.com>
2020-12-31 09:42:36 +0700

    removed countdown

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,8 +1,7 @@
 module.exports = function(client){
     console.log("Bot started!")
-    client.editStatus("online", {name: `${config.prefix}help || @${client.user.username}`, type: 5})
+    client.editStatus("online", {name: `${config.prefix}help || @${client.user.username} 🎄`, type: 5})
     require("../util/sdc")(client)
     require("../util/boticord")(client)
-    require("../util/countdown")(client)
     client.on("ready", () => console.log("Reconnected!"))
 }
