0fa14a94181ced3ea63990a63e8eefd11f9be720
RFX9870 <rru2005@gmail.com>
2021-01-07 12:07:07 +0700

    edited status

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,6 +1,6 @@
 module.exports = function(client){
     console.log("Bot started!")
-    client.editStatus("online", {name: `${config.prefix}help || @${client.user.username} 🎄`, type: 5})
+    client.editStatus("online", {name: `${config.prefix}help || @${client.user.username}`, type: 5})
     require("../util/sdc")(client)
     require("../util/boticord")(client)
     client.on("ready", () => console.log("Reconnected!"))
